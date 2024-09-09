c2bf77046cdf5ecc8c776b18daa2e2d88c530c25
SG64ru <sergg64@gmail.com>
2024-09-09 21:21:11 +0500

    Add files via upload

---
+++ Peremen.py
@@ -0,0 +1,6 @@
+Kol_DZ = 12
+Kol_chas = 1.5
+Name_curs = "Python,"
+Time_1_zadacha = Kol_chas / Kol_DZ
+print (Time_1_zadacha)
+print ("Курс:" + Name_curs, "всего задач:" + str (Kol_DZ), "затрачено часов" +", " + str (Kol_chas),", ", "среднее время выполнения" + " " + str (Time_1_zadacha))
